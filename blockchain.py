766ab81f45e1ab049b914ecf3ddf3eb0967e7b35
MYavuzYAGIS <m.yavuzyagis@gmail.com>
2022-02-24 22:08:20 +0300

    create

---
+++ blockchain.py
@@ -0,0 +1,20 @@
+import datetime
+import hashlib
+import json
+
+from flask import Flask, jsonify, request
+
+
+# Part 1 - Building a Blockchain
+class Blockchain:
+    def __init__(self):
+        
+
+
+
+
+
+
+
+
+# Part 2 - Mining our Blockchain
