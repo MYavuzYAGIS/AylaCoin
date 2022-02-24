c452ee88044e996b0922a9ee3234ce52450fc252
MYavuzYAGIS <m.yavuzyagis@gmail.com>
2022-02-24 22:30:22 +0300

    create POW function

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import datetime
 import hashlib
 import json
+from string import hexdigits
 
 from flask import Flask, jsonify, request
 
@@ -23,6 +24,17 @@ class Blockchain:
     
     def get_previous_block(self):
         return self.chain[-1] # returns the last block in the chain.
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1 # increment will need in each iteration until getting the right proof
+        check_proof = False 
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2-previous_proof**2 - 2**10).encode()).hexdigest() # adding b' in the head.
+            if hash_operation[:5] == '00000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
 
 
 
