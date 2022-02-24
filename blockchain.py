5282af2b09aaabfac2aa2310e74cf23a4f65ae14
MYavuzYAGIS <m.yavuzyagis@gmail.com>
2022-02-24 22:10:38 +0300

    tata:

--- blockchain.py
+++ blockchain.py
@@ -8,7 +8,8 @@ from flask import Flask, jsonify, request
 # Part 1 - Building a Blockchain
 class Blockchain:
     def __init__(self):
-        
+        self.chain = []
+        self.create_block(proof=1, previous_hash='0')  # genesis block and proof =1 is the proof of work. 1 is arbitrary number.
 
 
 
