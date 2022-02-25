4482bd5986f84ee11a14a735a66b9dc6c2fb0d6e
MYavuzYAGIS <m.yavuzyagis@gmail.com>
2022-02-25 22:42:59 +0300

    crate node interactions

--- aylaCoin.py
+++ aylaCoin.py
@@ -1,43 +1,47 @@
 import datetime
 import hashlib
 import json
+import uuid
 from http.client import HTTPResponse
+from sys import implementation
+from urllib.parse import urlparse
+from uuid import uuid4
 
+import requests
 from flask import Flask, jsonify, request
 
 
-# Part 1 - Building a Blockchain
 class Blockchain:
     def __init__(self):
         self.chain = []
-        self.create_block(
-            proof=1, previous_hash="0"
-        )  # genesis block and proof =1 is the proof of work. 1 is arbitrary number.
+        self.transactions = []
+        self.create_block(proof=1, previous_hash="0")
+        self.nodes = set()  # set is cheaper on the ram compared to list.
 
     def create_block(self, proof, previous_hash):
         block = {
-            "index": len(self.chain) + 1,  # index is the number of blocks in the chain.
-            "timestamp": str(
-                datetime.datetime.now()
-            ),  # timestamp is the time of the block creation.
-            "proof": proof,  # proof is the proof of work.
-            "previous_hash": previous_hash,  # previous_hash is the hash of the previous block.
+            "index": len(self.chain) + 1,
+            "timestamp": str(datetime.datetime.now()),
+            "proof": proof,
+            "previous_hash": previous_hash,
+            "transactions": self.transactions,
         }
+        self.transactions = (
+            []
+        )  # after adding the transactions to the block, I need to empty the list so no repetition in the block registry.
         self.chain.append(block)
         return block
 
     def get_previous_block(self):
-        return self.chain[-1]  # returns the last block in the chain.
+        return self.chain[-1]
 
     def proof_of_work(self, previous_proof):
-        new_proof = (
-            1  # increment will need in each iteration until getting the right proof
-        )
+        new_proof = 1
         check_proof = False
         while check_proof is False:
             hash_operation = hashlib.sha256(
                 str(new_proof**2 - previous_proof**2 - 2**10).encode()
-            ).hexdigest()  # adding b' in the head.
+            ).hexdigest()
             if hash_operation[:5] == "00000":
                 check_proof = True
             else:
@@ -66,10 +70,43 @@ class Blockchain:
             block_index += 1
         return True
 
+    def add_transaction(self, sender, receiver, amount):
+        self.transactions.append(
+            {"sender": sender, "receiver": receiver, "amount": amount}
+        )
+        previous_block = self.get_previous_block()
+        return (
+            previous_block["index"] + 1
+        )  # last block is aleady finished working. Hence, i need the one after the last one.
 
-# Mining Blockchain
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
 
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f"http://{node}/get_chain/")
+            if response.status_code == 200:
+                length = response.json()["length"]
+                chain = response.json()["chain"]
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
+
+
+# ====== implementation =======
 blockchain = Blockchain()
+
+# Create an address for the node on port 5000
+node_address = str(uuid4()).replace("-", "")
+
 
 app = Flask(__name__)
 
@@ -81,6 +118,7 @@ def mine_block():
     previous_block = blockchain.get_previous_block()
     previous_proof = previous_block["proof"]
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender=node_address, receiver='Ayla',amount=100)
     proof = blockchain.proof_of_work(previous_proof)
     block = blockchain.create_block(
         proof, previous_hash
@@ -91,11 +129,9 @@ def mine_block():
         "timestamp": block["timestamp"],
         "proof": block["proof"],
         "previous_hash": block["previous_hash"],
+        "transactions": block["transactions"],
     }
     return jsonify(response), 200
-
-
-# get full blockchain.
 
 
 @app.route("/get_chain", methods=["GET"])
