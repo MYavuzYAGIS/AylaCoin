3447d99a5c2610d264fee2d90e315f0cb1abe03f
MYavuzYAGIS <m.yavuzyagis@gmail.com>
2022-02-25 22:24:40 +0300

    add replace_chain function to find the longest chain based on the length comparison of the nodes in the network

--- customCurrency.py
+++ customCurrency.py
@@ -3,6 +3,7 @@ import hashlib
 import json
 import uuid
 from http.client import HTTPResponse
+from sys import implementation
 from urllib.parse import urlparse
 from uuid import uuid4
 
@@ -14,8 +15,8 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.transactions = []
-        self.create_block(proof=1, previous_hash='0')  
-
+        self.create_block(proof=1, previous_hash='0')
+        self.nodes = set()  # set is cheaper on the ram compared to list.
     def create_block(self,proof,previous_hash):
         block = {
             'index' : len(self.chain) + 1, 
@@ -27,11 +28,8 @@ class Blockchain:
         self.transactions = []  # after adding the transactions to the block, I need to empty the list so no repetition in the block registry. 
         self.chain.append(block)
         return block
-
     def get_previous_block(self):
         return self.chain[-1] 
-    
-
     def proof_of_work(self, previous_proof):
         new_proof = 1 
         check_proof = False 
@@ -42,11 +40,9 @@ class Blockchain:
             else:
                 new_proof += 1
         return new_proof
-    
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
-    
     def is_chain_valid(self,chain):
         previous_block = chain[0]
         block_index = 1
@@ -70,6 +66,35 @@ class Blockchain:
             })
         previous_block = self.get_previous_block()
         return previous_block['index'] + 1 # last block is aleady finished working. Hence, i need the one after the last one.
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'http://{node}/get_chain/')
+            if response.status_code == 200:
+               length = response.json()['length']
+               chain = response.json()['chain']
+               if length > max_length and self.is_chain_valid(chain):
+                   max_length = length
+                   longest_chain = chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
+                   
+
+
+
+
+
+
+# ====== implementation =======
+
+
 blockchain = Blockchain()
 
 app = Flask(__name__)
@@ -103,9 +128,6 @@ def get_chain():
         'length':len(blockchain.chain) 
     }
     return jsonify(response), 200    
-
-
-
 # check whether if the blockchain is valid
 
 @app.route('/validate',methods=["GET"])
@@ -117,7 +139,5 @@ def validate():
     else:
         response = "Not  a Valid chain"
     return response,200
-    
-
 
 app.run(host='0.0.0.0',port=5000)
