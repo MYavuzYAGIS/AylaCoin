08e79640181c84e5f822b34e6f2c346fdac5c112
MYavuzYAGIS <m.yavuzyagis@gmail.com>
2022-06-09 13:38:16 +0300

    add SPDX license

--- aylacoin_ico.sol
+++ aylacoin_ico.sol
@@ -1,3 +1,4 @@
+//"SPDX-License-Identifier: GPL-3.0-or-later
 // Aylacoin'n ICO
 
 //version
@@ -6,4 +7,9 @@ pragma solidity ^0.8.14;
 contract aylacoin_ico {
     // Introduce the max num of aylacoin for sale
     uint32 public max_aylacoins = 1000000;
+
+    // conversion USD/AC parity
+    uint256 public usd_to_aylacoin = 100;
+
+    //
 }
